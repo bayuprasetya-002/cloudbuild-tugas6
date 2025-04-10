a174116a1ec636c3921a740a07c9ef1e51712a77
Bayu Prasetya Wijaya <161041397+bayuprasetya-002@users.noreply.github.com>
2025-04-10 18:49:10 +0700

    Update database.js

--- backendnotes/config/database.js
+++ backendnotes/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize("notes", "root", "notes123", {
-    host: "107.178.221.91",
+    host: "34.45.72.91",
     dialect: "mysql",
     timezone: "+07:00"
 }) 
